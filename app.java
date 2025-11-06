990d8b3d6873c78e0beedec3f92e60df5c639385
Bharathchowdary <bharathchowdary718@gmail.com>
2025-11-06 19:12:19 +0530

    Dev-2 added code

--- app.java
+++ app.java
@@ -3,3 +3,12 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }   
+
+// dev-2 push form local
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This line prints "Hello, World!" to the console
+        System.out.println("Hello, World!"); 
+    }
+}
